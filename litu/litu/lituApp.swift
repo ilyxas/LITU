427ff20004848b52c195e8d8475ac15fbe48abd8
ilyxas <ilyxas@gmail.com>
2026-04-16 18:54:13 +0300

    full

--- litu/litu/lituApp.swift
+++ litu/litu/lituApp.swift
@@ -1,17 +1,1 @@
-//
-//  lituApp.swift
-//  litu
-//
-//  Created by ilya on 15/04/2026.
-//
-
-import SwiftUI
-
-@main
-struct lituApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
+import SwiftUI@mainstruct LituApp: App {    var body: some Scene {        WindowGroup {            RootView()        }    }}
